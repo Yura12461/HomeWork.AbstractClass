62b6da14537278e1ce0d961016be047226c9e20c
Юрий Гущин <yuriy.gushchin.01@mail.ru>
2024-04-11 11:01:29 +0300

    Добавьте файлы проекта.

---
+++ HomeWork.AbstractClass/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+using System.Runtime.CompilerServices;
+
+namespace Animal
+{
+
+
+    abstract class Animal
+    {
+        public string Name { get; set; }
+       
+
+
+
+
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Console.WriteLine("Введите имя собаки");
+            dog.SetName();
+            GetName(dog);
+            dog.Eat();
+        }
+        
+        public void SetName()
+        {
+        
+            Name = Console.ReadLine();
+        }
+
+        public static void GetName(Animal dog)
+        {
+
+           
+            Console.WriteLine($"Имя собаки: {dog.Name} ");
+        }
+
+        public abstract void Eat();
+        
+        
+
+        
+
+    }
+
+    class Dog : Animal 
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Собака ест");
+        }
+
+        
+    }
+    
+       
+
+       
+
+        
+    
+}
